0be13e57c3249238af9bbe9432676f778f15270d
Thiago Gomes <thigstudy@gmail.com>
2026-09-02 09:34:20 -0300

    feat: spring initializr

---
+++ src/main/java/com/fleetpulse/FleetpulseApplication.java
@@ -0,0 +1,13 @@
+package com.fleetpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetpulseApplication.class, args);
+	}
+
+}
