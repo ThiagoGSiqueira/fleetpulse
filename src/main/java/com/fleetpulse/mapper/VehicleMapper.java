b10f8f9fa515cba0e4cf99e50f1fd08c9ca15c71
Thiago Gomes <thigstudy@gmail.com>
2026-09-11 11:28:17 -0300

    feat(vehicle): add vehicle mapper

---
+++ src/main/java/com/fleetpulse/mapper/VehicleMapper.java
@@ -0,0 +1,23 @@
+package com.fleetpulse.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.fleetpulse.domain.Vehicle;
+import com.fleetpulse.web.dto.VehicleRequestDto;
+import com.fleetpulse.web.dto.VehicleResponseDto;
+
+@Component 
+public class VehicleMapper {
+    public VehicleResponseDto toDto(Vehicle vehicleEntity) {
+        return new VehicleResponseDto(vehicleEntity.getId(), vehicleEntity.getLicensePlate(), vehicleEntity.getModel(), 
+        vehicleEntity.getStatus());
+    }
+
+    public Vehicle toEntity(VehicleRequestDto vehicleDto) {
+        Vehicle vehicleEntity = new Vehicle();
+        vehicleEntity.setLicensePlate(vehicleDto.licensePlate());
+        vehicleEntity.setModel(vehicleDto.model());
+
+        return vehicleEntity;
+    }
+}
