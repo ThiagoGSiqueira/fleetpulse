9c476d669634605425df7a1be60753b2fbefd8b9
Thiago Gomes <thigstudy@gmail.com>
2026-09-18 08:49:30 -0300

    feat(driver): prevent disabling driver when on a trip

--- src/main/java/com/fleetpulse/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fleetpulse/exception/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
